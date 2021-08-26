679c9180440ca460104c998f2e5cca73da61ffa8
yws <yws528819@163.com>
2021-08-27 00:07:53 +0800

    逆波兰表达式 完善

--- src/stack/PolandNotation.java
+++ src/stack/PolandNotation.java
@@ -8,14 +8,86 @@ public class PolandNotation {
 
     public static void main(String[] args) {
 
-        String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
 
-        List<String> list = getListString(suffixExpression);
+        String expression = "1+((2+3)*4)-5";
+        List<String> infiExpressionList = toInfiExpressionList(expression);
 
-        int value = calculate(list);
+        List<String> suffixExpressionList =  transferToSuffixExpression(infiExpressionList);
 
-        System.out.println(value);
 
+
+        // String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
+        //
+        // List<String> list = getListString(suffixExpression);
+        //
+        // int value = calculate(list);
+        //
+        // System.out.println(value);
+
+    }
+
+    private static List<String> transferToSuffixExpression(List<String> infiExpressionList) {
+        //符号栈
+        Stack<String> operStack = new Stack<>();
+        List<String> result = new ArrayList<>();
+        String oper = null;
+
+        for (int i=0; i<infiExpressionList.size(); i++) {
+            String item = infiExpressionList.get(i);
+            if (item.matches("\\d+")) {
+                result.add(item);
+            }else {
+                if (item.equals("(")) {
+                    operStack.push(item);
+                }else if (item.equals(")")) {
+                    while (!(oper = operStack.pop()).equals("(")) {
+                        result.add(oper);
+                    }
+                }else {
+                    while (operStack.size() > 0 && Operation.getValue(item) <= Operation.getValue(oper = operStack.pop())) {
+                        result.add(oper);
+                    }
+                    operStack.push(oper);
+                }
+            }
+
+        }
+
+        while (!operStack.isEmpty()) {
+            result.add(operStack.pop());
+        }
+
+        return result;
+    }
+
+
+    /**
+     * 将中缀表达式转成对应的list
+     * @param expression
+     * @return
+     */
+    private static List<String> toInfiExpressionList(String expression) {
+
+        int i=0;
+        List<String> infiList = new ArrayList<>();
+
+        while(i < expression.length()) {
+            char c = expression.charAt(i);
+            //如果不是数字
+            if(c < 48 || c > 57) {
+                //直接放入集合
+                infiList.add("" + c);
+            }else {
+                String num = "" + c;
+                //考虑多位数，如果下一个字符还是数字，就拼接
+                while (i+1 < expression.length() && expression.charAt(i + 1) >=48 && expression.charAt(i + 1) <= 57) {
+                    num += expression.charAt(++i);
+                }
+                infiList.add(num);
+            }
+            i++;
+        }
+        return infiList;
     }
 
     private static int calculate(List<String> list) {
@@ -59,5 +131,35 @@ public class PolandNotation {
         return list;
     }
 
+
+}
+
+class Operation{
+    private static final int ADD = 1;
+    private static final int SUB = 1;
+    private static final int MUTI = 2;
+    private static final int DIV = 2;
+
+    public static int getValue(String operate) {
+        int res = 0;
+        switch (operate) {
+            case "+":
+                res = ADD;
+                break;
+            case "-":
+                res = SUB;
+                break;
+            case "*":
+                res = MUTI;
+                break;
+            case "/":
+                res = DIV;
+                break;
+            default:
+                System.out.println("不存在运算符:" + operate);
+                break;
+        }
+        return res;
+    }
 
 }
