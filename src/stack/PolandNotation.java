733d175593c71a03eafb33ce2f2c9104bb1910f8
yws <yws528819@163.com>
2021-08-30 16:40:35 +0800

    逆波兰表达式完善

--- src/stack/PolandNotation.java
+++ src/stack/PolandNotation.java
@@ -8,30 +8,47 @@ public class PolandNotation {
 
     public static void main(String[] args) {
 
-
         String expression = "1+((2+3)*4)-5";
+        //将中缀表达式转成对应的list（拆分字符串，区分是符号还是多位数字）
         List<String> infiExpressionList = toInfiExpressionList(expression);
-
+        //将中缀表达式集合转成后缀表达式（逆波兰表达式）集合
         List<String> suffixExpressionList =  transferToSuffixExpression(infiExpressionList);
+        System.out.println(suffixExpressionList);
 
+        //String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
+        //将中缀表达式字符串（带空格，用空格拆分）转成list
+        //List<String> list = getListString(suffixExpression);
 
-
-        // String suffixExpression = "4 5 * 8 - 60 + 8 2 / +";
-        //
-        // List<String> list = getListString(suffixExpression);
-        //
-        // int value = calculate(list);
-        //
-        // System.out.println(value);
+        //计算后缀表达式集合
+        int value = calculate(suffixExpressionList);
+        System.out.println(value);
 
     }
 
+    /**
+     * 将中缀表达式集合转成后缀表达式（逆波兰表达式）集合
+     * @param infiExpressionList
+     * @return
+     */
     private static List<String> transferToSuffixExpression(List<String> infiExpressionList) {
         //符号栈
         Stack<String> operStack = new Stack<>();
         List<String> result = new ArrayList<>();
         String oper = null;
-
+        /**
+         * 参数：1个中缀表达式集合
+         * 处理使用：1个结果集合，1个栈（符号栈）
+         * 处理过程：遍历中缀表达式集合的元素
+         * 1) 如果是数字，直接放入集合里
+         * 2) 如果是左括号(，直接放入符号栈里
+         * 3) 如果是右括号)，将符号栈里的符号一一出栈放入集合里，直到左括号弹出
+         * 4) 如果是计算符号，就比较栈里符号优先度是否比当前元素符号高或者相等，
+         *      如果是，栈弹出，放入集合里，
+         *      再比较栈后面的符号，重复操作，直到栈里的优先度比元素符号的小
+         *      如果小，就将当前元素符号入符号栈
+         * 5) 遍历完，如果符号栈还有元素，一一出栈，放入结果集合里
+         *
+         */
         for (int i=0; i<infiExpressionList.size(); i++) {
             String item = infiExpressionList.get(i);
             if (item.matches("\\d+")) {
@@ -44,10 +61,16 @@ public class PolandNotation {
                         result.add(oper);
                     }
                 }else {
-                    while (operStack.size() > 0 && Operation.getValue(item) <= Operation.getValue(oper = operStack.pop())) {
-                        result.add(oper);
+                    //第一个符号，符号栈里是空的
+                    if (operStack.isEmpty()) {
+                        oper = item;
+                    }else {
+                        while (operStack.size() > 0 && Operation.getValue(item) <= Operation.getValue(oper = operStack.peek())) {
+                            operStack.pop();
+                            result.add(oper);
+                        }
                     }
-                    operStack.push(oper);
+                    operStack.push(item);
                 }
             }
 
@@ -62,7 +85,7 @@ public class PolandNotation {
 
 
     /**
-     * 将中缀表达式转成对应的list
+     * 将中缀表达式转成对应的list（拆分字符串，区分是符号还是多位数字）
      * @param expression
      * @return
      */
@@ -90,9 +113,19 @@ public class PolandNotation {
         return infiList;
     }
 
+    /**
+     * 计算后缀表达式集合
+     * @param list
+     * @return
+     */
     private static int calculate(List<String> list) {
         Stack<String> stack = new Stack();
 
+        /**
+         * 将集合（符号或者数字）一个个元素，放到一个栈里
+         * 如果是数字，直接入栈
+         * 如果是符号，就出栈2个数字，进行该符号的计算，将计算结果入栈（后缀表达式已经解决好计算的顺序）
+         */
         for (String item : list) {
             if (item.matches("\\d+")) {
                 stack.push(item);
